79a49bc275e614e6b93811cf249dd727f54d5662
Gloria Walls <gloria.walls@g.austincc.edu>
2026-02-13 01:31:22 -0600

    define instance variables

---
+++ my-docker-java-monster/Monster.java
@@ -0,0 +1,14 @@
+/*  Monster.java
+ *  COSC 4301
+ *  Gloria Walls
+ *  2/13/2026
+ *  Purpose: This is monster class for
+ *           the Neon Ark project
+ */
+
+public class Monster
+{
+    private String name;
+    private String type;
+
+}
