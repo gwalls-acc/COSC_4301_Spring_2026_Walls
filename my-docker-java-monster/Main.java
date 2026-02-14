4c7c2b31228c935b81826392859a7d99aa93e18a
Gloria Walls <gloria.walls@g.austincc.edu>
2026-02-14 07:05:15 -0600

    Adding comments to Monster and Main classes

--- my-docker-java-monster/Main.java
+++ my-docker-java-monster/Main.java
@@ -1,5 +1,12 @@
+/*  Main.java
+ *  COSC 4301
+ *  Gloria Walls
+ *  2/13/2026
+ *  Purpose: This is a main class for instantiating a monster object
+ */
 public class Main
 {
+    // main method
     public static void main(String[] args)
     {
         Monster myMonster = new Monster("Abaia","Water");
