a2eb3e3c25aca46499a019cb3eed2c460c428412
Gloria Walls <gloria.walls@g.austincc.edu>
2026-02-14 07:05:46 -0600

    Adding comments to Monster and Main classes

--- my-docker-java-monster/Monster.java
+++ my-docker-java-monster/Monster.java
@@ -8,15 +8,19 @@
 
 public class Monster
 {
+
+   // name and type variables
     private String name;
     private String type;
 
+    //Monster class contructor
     public Monster(String name, String type)
     {
         this.name= name;
         this.type = type;
     }
 
+    //method to display message about the name and type variables
     public String getDescription()
     {
         return name + "is a " + type + "-type monster from the Neon Ark training program.";
